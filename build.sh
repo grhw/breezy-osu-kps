72ae7d933948a02cfe1cb240daf7394e77af6ac1
Gust Who? <lucgustga@outlook.com>
2025-03-19 02:41:52 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,23 +1,9 @@
+rm -r dist/
 mkdir dist/
-echo "# -   Building for Linux   - #"
-mkdir dist/linux
 
-nuitka --onefile --standalone --output-dir=dist/linux/ \
+nuitka --mode=app --output-dir=dist/ \
        --enable-console --windows-icon-from-ico=src/internal/icon.ico \
        --output-filename=BreezyKPS src/main.py
 
-cp src/internal dist/linux/internal
-cp default_config.json dist/linux/config.json
-echo "# -   Done building for Linux   - #"
-
-echo "# -   Building for Windows   - #"
-mkdir dist/windows
-
-nuitka --mingw64 --windows-target \
-       --onefile --standalone --output-dir=dist/linux/ \
-       --enable-console --windows-icon-from-ico=src/internal/icon.ico \
-       --output-filename=BreezyKPS.exe src/main.py
-
-cp src/internal dist/windows/internal
-cp default_config.json dist/windows/config.json
-echo "# -   Done building for Windows   - #"
+cp -r src/internal dist/internal
+cp default_config.json dist/config.json
