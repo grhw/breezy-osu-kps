f8317a94c7327939c35350124beab58d47a6e163
Gust Who? <lucgustga@outlook.com>
2025-03-18 09:49:18 +0800

    no error

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def send_ui():
         config = json.loads(f.read())
     for i in config["keys"]:
         pressed_keys[i.upper()] = False
-    return render_template("index.html",v=str(config["visualizer_enabled"]).lower(),vs=config["visualizer_speed"],k=config["keys"].upper())
+    return render_template("index.html",v="y" if config["visualizer_enabled"] else "",vs=config["visualizer_speed"],k=config["keys"].upper())
 
 @server.route("/assets/<file>")
 def assets(file):
